ff03b5c03a07b0e39feef242a5cfa9bd035d772d
Prashant Soni <prashantsonietawah@gmail.com>
2022-09-21 00:27:16 +0530

    intial commit pf react portfolio backup

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,13 @@
+import React from "react";
+import LoaderContainer from "../../containers/LoaderContainer";
+import Prashant from "../../assets/images/Prashant";
+
+const Loader = () => {
+    return (
+        <LoaderContainer>
+            <Prashant width={250} />
+        </LoaderContainer>
+    );
+};
+
+export default Loader;
